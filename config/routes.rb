a6e754348422bdac90dbacb56fc418fe375cb9a7
Nguyen3464 <n.nguyen3464@gmail.com>
2023-04-19 15:53:49 -0700

    Blog show submission

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'blogs' => 'blog#index'
   root 'blog#index'
+  get 'blogs' => 'blog#index', as: 'home'
+  get 'blogs/:id' => 'blog#show', as: 'blog_name'
+  get 'blogs' => 'blog#content'
 end
