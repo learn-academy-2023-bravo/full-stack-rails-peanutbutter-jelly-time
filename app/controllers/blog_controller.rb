9ec77c082cf9cbc63b2857aadda7d83c114de4b6
Nguyen3464 <n.nguyen3464@gmail.com>
2023-04-20 10:36:39 -0700

    blog create submision

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -3,15 +3,24 @@ class BlogController < ApplicationController
         @blogs = Blog.all
     end
 
-    def content
-        @content = Blog.all
-    end
-
     def show
-        @blogs = Blog.find(params[:id])
+        @blog = Blog.find(params[:id])
     end
 
     def new
-        @blogs = Blog.new
+        @blog = Blog.new
     end
+
+    def create
+        @blog = Blog.create(blog_params)
+        if @blog.valid?
+            redirect_to blogs_path
+        end
+    end
+
+    private
+    def blog_params 
+        params.require(:blog).permit(:title, :content)
+    end
+
 end
