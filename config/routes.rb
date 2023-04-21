e16826aaf00a165c015a7d91c800ffab935f12ce
Ealvarez01 <e.alvarez2404@yahoo.com>
2023-04-20 20:53:31 -0700

    blog-edit story almost complete working on create

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   root 'blog#index'
+
   get 'blogs' => 'blog#index', as: 'blogs'
+  
   get 'blogs/new' => 'blog#new', as: 'new_blog_entry'
-  post 'blogs' => 'blog#create'
+
   get 'blogs/:id' => 'blog#show', as: 'blog_name'
+
+  post 'blogs' => 'blog#create'
+
+  get 'blogs/:id/edit' => 'blog#edit', as: 'edit_blog'
+  
   delete 'blogs/:id' => 'blog#destroy', as: 'delete_blog'
+  
 end
